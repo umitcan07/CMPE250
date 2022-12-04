55b89875ff897ed5c0741a3096cb906f01aa3711
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2022-12-04 09:33:20 +0300

    Project4 Initialization & Incomplete Project3

---
+++ Project3/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Flight> readyQ = new PriorityQueue<>();
+    }
+}
